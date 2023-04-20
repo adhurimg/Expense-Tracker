736db72ecac09f5d2556b4f9f15e65e1b95071c2
adhurimg <adhurimgashi03@gmail.com>
2023-04-20 03:10:27 +0200

    Create GlobalStyles.js

---
+++ frontend/src/styles/GlobalStyles.js
@@ -0,0 +1,14 @@
+import {createGlobal, createGlobalStyle} from 'styled-components'
+
+export const GlobalStyle = createGlobalStyle` 
+    *{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+    list-style: none; 
+    }
+    
+    :root {
+    
+    }
+    `;
