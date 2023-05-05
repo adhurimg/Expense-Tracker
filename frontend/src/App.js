d13c7d6bfbfc46dcc3feb61820a49d5c00a83ba0
adhurimg <adhurimgashi03@gmail.com>
2023-05-05 15:08:38 +0200

    Worked on App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,51 @@
 import React, {useState, useMemo} from 'react'
 import styled from "styled-components";
-
+import bg from './img/bg.png'
+import {MainLayout} from './styles/Layouts'
+import Orb from './Components/Orb/Orb'
+import Navigation from './Components/Navigation/Navigation'
+import Dashboard from './Components/Dashboard/Dashboard';
+import Income from './Components/Income/Income'
+import Expenses from './Components/Expenses/Expenses';
+import { useGlobalContext } from './context/globalContext';
 function App() {
-  return (
-    <div className="App">
+  const [active, setActive] = useState(1)
 
-    </div>
+  const global = useGlobalContext()
+  console.log(global);
+
+  const displayData = () => {
+    switch(active){
+      case 1:
+        return <Dashboard />
+      case 2:
+        return <Dashboard />
+      case 3:
+        return <Income />
+      case 4:
+        return <Expenses />
+      default:
+        return <Dashboard />
+    }
+  }
+
+  const orbMemo = useMemo(() => {
+    return <Orb />
+  },[])
+
+  return (
+      <AppStyled bg={bg} className="App">
+        {orbMemo}
+        <MainLayout>
+          <Navigation active={active} setActive={setActive} />
+          <main>
+            {displayData()}
+          </main>
+        </MainLayout>
+      </AppStyled>
   );
 }
+
 
 const AppStyled = styled.div`
   height: 100vh;
